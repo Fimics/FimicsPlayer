8045544207237eed7fb8104f909cb88e98e13a5e
fimics <lpjhblpj@163.com>
2020-03-31 03:32:38 +0800

    nav

--- app_player/src/main/java/com/mic/jetpack/MainJetpackActivity.java
+++ app_player/src/main/java/com/mic/jetpack/MainJetpackActivity.java
@@ -3,18 +3,27 @@ package com.mic.jetpack;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mic.R;
 import com.mic.core.utils.StatusBarUtil;
+import com.mic.jetpack.utils.NavGraphBuilder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainJetpackActivity extends AppCompatActivity {
+public class MainJetpackActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+   private NavController navController;
 
     public static void start(Activity activity){
         Intent intent = new Intent(activity, MainJetpackActivity.class);
@@ -23,22 +32,25 @@ public class MainJetpackActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+//        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_jetpack);
-        StatusBarUtil.setStatusBarColor(this, getResources().getColor(R.color.colorPrimary));
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+//        StatusBarUtil.setStatusBarColor(this, getResources().getColor(R.color.colorPrimary));
 
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = NavHostFragment.findNavController(fragment);
+//        NavigationUI.setupWithNavController(navView, navController);
+
+        NavGraphBuilder.build(this,navController,fragment.getId());
+        navView.setOnNavigationItemSelectedListener(this);
         //处理DeepLink
-        navController.handleDeepLink(getIntent());
+       // navController.handleDeepLink(getIntent());
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        navController.navigate(item.getItemId());
+        return TextUtils.isEmpty(item.getTitle());
+    }
 }
--- app_player/src/main/java/com/mic/jetpack/model/Destination.java
+++ app_player/src/main/java/com/mic/jetpack/model/Destination.java
@@ -11,58 +11,11 @@ public class Destination {
      * id : 1305792916
      */
 
-    private boolean isFragment;
-    private boolean asStarter;
-    private boolean needLogin;
-    private String pageUrl;
-    private String className;
-    private int id;
+    public boolean isFragment;
+    public boolean asStarter;
+    public boolean needLogin;
+    public String pageUrl;
+    public String className;
+    public int id;
 
-    public boolean isIsFragment() {
-        return isFragment;
-    }
-
-    public void setIsFragment(boolean isFragment) {
-        this.isFragment = isFragment;
-    }
-
-    public boolean isAsStarter() {
-        return asStarter;
-    }
-
-    public void setAsStarter(boolean asStarter) {
-        this.asStarter = asStarter;
-    }
-
-    public boolean isNeedLogin() {
-        return needLogin;
-    }
-
-    public void setNeedLogin(boolean needLogin) {
-        this.needLogin = needLogin;
-    }
-
-    public String getPageUrl() {
-        return pageUrl;
-    }
-
-    public void setPageUrl(String pageUrl) {
-        this.pageUrl = pageUrl;
-    }
-
-    public String getClassName() {
-        return className;
-    }
-
-    public void setClassName(String className) {
-        this.className = className;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
