695359a8d60151816a9492e2149996a6286a50b9
fimics <lpjhblpj@163.com>
2020-04-06 01:39:48 +0800

    refactor layout

---
+++ app_player/src/main/java/com/mic/ui/my/MyFragment.java
@@ -0,0 +1,69 @@
+package com.mic.ui.my;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import com.mic.R;
+import com.mic.databinding.FragmentMyBinding;
+import com.mic.core.utils.StatusBar;
+import com.mic.tabs.model.User;
+import com.mic.ui.login.UserManager;
+
+public class MyFragment extends Fragment {
+    private FragmentMyBinding mBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mBinding = FragmentMyBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        User user = UserManager.get().getUser();
+        mBinding.setUser(user);
+
+        UserManager.get().refresh().observe(this, newUser -> {
+            if (newUser != null) {
+                mBinding.setUser(newUser);
+            }
+        });
+
+        mBinding.actionLogout.setOnClickListener(v -> new AlertDialog.Builder(getContext())
+                .setMessage(getString(R.string.fragment_my_logout))
+                .setPositiveButton(getString(R.string.fragment_my_logout_ok), (dialog, which) -> {
+                    dialog.dismiss();
+                    UserManager.get().logout();
+                    getActivity().onBackPressed();
+                }).setNegativeButton(getString(R.string.fragment_my_logout_cancel), null)
+                .create().show());
+
+        mBinding.goDetail.setOnClickListener(v -> ProfileActivity.startProfileActivity(getContext(), ProfileActivity.TAB_TYPE_ALL));
+        mBinding.userFeed.setOnClickListener(v -> ProfileActivity.startProfileActivity(getContext(), ProfileActivity.TAB_TYPE_FEED));
+        mBinding.userComment.setOnClickListener(v -> ProfileActivity.startProfileActivity(getContext(), ProfileActivity.TAB_TYPE_COMMENT));
+        mBinding.userFavorite.setOnClickListener(v -> UserBehaviorListActivity.startBehaviorListActivity(getContext(), UserBehaviorListActivity.BEHAVIOR_FAVORITE));
+        mBinding.userHistory.setOnClickListener(v -> UserBehaviorListActivity.startBehaviorListActivity(getContext(), UserBehaviorListActivity.BEHAVIOR_HISTORY));
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        StatusBar.lightStatusBar(getActivity(), false);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        StatusBar.lightStatusBar(getActivity(), hidden);
+    }
+}
